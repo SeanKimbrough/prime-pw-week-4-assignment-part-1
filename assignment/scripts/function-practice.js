39a2582eeeb78969f42084fb77a768cf689eafaf
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-10 18:57:01 -0600

    finished assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -69,22 +69,30 @@ let fruit = ['apple','banana','orange','pear'];
 fruit[fruit.length-1];
 console.log('last Item is array:' , fruit.length-1);
 
+
+
   
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ) {
-  for (let i=0; i > array.length; i++){
-    const topValue = array [i];
-  }
-}
-console.log (' Top Value:' , topValue);
+ function find( value, array ) {
+   for (let i=0; i > array.length; i++){
+     const individualValue = array[i];
+console.log('individual value:' , individualValue);
 
-if (topValue === value ) {
+if (individualValue === value ){
   return true;
 }
-return false
+   }
+   return false;
+
+ }
+ const listValues= [3,1,8,2,6,4];
+ console.log('the function should return true' , find(3, listValues));
+console.log('the function should return false' , find(listValues, 10));
+console.log('the function should return true' , find(2, listValues));
+
 
 
 
@@ -94,17 +102,28 @@ return false
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  if (letter === string[0]){
+    return true;
+
+  }
+  return false;
 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll(  array) {
+  let sum = 0;
+  for( i=0; i<array.length; i++){
+    sum=array[i] + sum
+
+  }
   // TODO: loop to add items
   return sum;
 }
+const numbers =[4,7,3,2,1,0];
+console.log('the sum of the numbers is:' , sumAll(numbers));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
